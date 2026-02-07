3381e622e40ad3a6dbe2abadfa3559a722fd3b59
csdevmustafa-ui <cs.dev.mustafa@gmail.com>
2026-02-08 00:42:57 +0300

    Update app.js

--- assets/app.js
+++ assets/app.js
@@ -2,8 +2,7 @@
 window.HATHER_API_URL = "https://script.google.com/macros/s/AKfycbyYrlS2KNKfACmTuOHhhOvSaYK-QM5M7CFqfDFZ8dgzl8Lrr-i9pSv2yxSCQL5Zrjak/exec";
 
 window.apiCall = async function(payload){
-  const url = window.HATHER_API_URL;
-  const res = await fetch(url, {
+  const res = await fetch(window.HATHER_API_URL, {
     method: "POST",
     body: JSON.stringify(payload || {})
   });
